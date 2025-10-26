eaddbc3348f1eb097603a96251978080fee17d8e
Kirutre <gabrielpernalete3108@gmail.com>
2025-10-25 22:16:24 -0400

    Finished without follow mode

--- radar.ino
+++ radar.ino
@@ -11,12 +11,20 @@ enum Limits {
   RIGHT = 179
 };
 
+enum Modes {
+  AUTOMATIC = 'a',
+  MANUAL = 'b',
+  FOLLOW = 'c'
+};
+
 Servo engine;
 
 const int delay_between_angle = 20;
 
 int current_engine_angle = 90;
 int direction_movement = 1;
+
+Modes current_mode = AUTOMATIC;
 
 void setup(){
   pinMode(TRIGGER, OUTPUT);
@@ -31,17 +39,44 @@ void setup(){
 }
 
 void loop(){
-  long duration;
-  long distance;
+  if (current_mode == MANUAL) {
+    direction_movement = 0;
+  } else if (current_mode == AUTOMATIC) {
+    if (direction_movement == 0) {
+      direction_movement = 1;
+    }
+    
+    calculate_engine_angle();
+  }
+  
+  if (Serial.available() > 0) {
+    char incoming_data = Serial.read();
+
+    if (incoming_data == AUTOMATIC) {
+      current_mode = AUTOMATIC;
+    } else if (incoming_data == MANUAL) {
+      current_mode = MANUAL;
+    } else if (incoming_data == FOLLOW) {
+      current_mode = FOLLOW;
+    } else if (incoming_data == '2') {
+      direction_movement = -1;
+
+      calculate_engine_angle();
+    } else if (incoming_data == '1') {
+      direction_movement = 1;
+
+      calculate_engine_angle();
+    } else if (incoming_data == '0') {
+      direction_movement = 0;
+    }
+  }
 
   digitalWrite(TRIGGER, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIGGER, LOW);
 
-  duration = pulseIn(ECHO, HIGH);
-  distance = duration / 58;
-  
-  calculate_engine_angle();
+  long duration = pulseIn(ECHO, HIGH);
+  long distance = duration / 58;
 
   engine.write(current_engine_angle);
 
